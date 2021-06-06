e00160b252356836af408d16036c6cbc96e9d12a
sandipanaich <sandipanaich@gmail.com>
2021-06-06 17:48:44 +0530

    Initial commit for package structures

---
+++ src/week1/algo/search/LinearSearch.java
@@ -0,0 +1,10 @@
+package week1.algo.search;
+
+public class LinearSearch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/week1/ds/stack/Stack.java
@@ -0,0 +1,10 @@
+package week1.ds.stack;
+
+public class Stack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
