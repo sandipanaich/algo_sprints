d604ce8396760d537d612c59a562f9f2e00328fe
sandipanaich <sandipanaich@gmail.com>
2021-06-14 09:44:17 +0530

    Initial Draft of Stack Implementation.

---
+++ src/week2/ds/stack/Stack.java
@@ -0,0 +1,31 @@
+/**
+ * Stack Implementation using resizing/dynamic arrays.
+ * peek: O(1)
+ * pop: 0(1)
+ * push: O(1)
+ */
+package week2.ds.stack;
+
+/**
+ * @author Sandipan Aich
+ */
+public class Stack<T> {
+
+	public static void main(String[] args) {
+
+//		Stack<Integer> stack = new Stack<>();
+//
+//		System.out.println("==== PUSH ==============");
+//		for (int i = 0; i < 20; i++) {
+//			stack.push(i);
+//			System.out.println("Data :: " + stack.peek() + " :: size :: " + stack.getSize() + " :: " + stack.LENGTH);
+//		}
+//		
+//		System.out.println("==== POP ===============");
+//		while (!stack.isEmpty()) {
+//			System.out.println("Data :: " + stack.peek() + " :: size :: " + stack.getSize() + " :: " + stack.LENGTH);
+//			stack.pop();
+//		}
+	}
+
+}
