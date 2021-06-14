47eaa1b45ff7c2d5e70f5aea154b1a6680192933
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2021-06-14 10:04:53 +0530

    Weighted Quick Find Implementation

--- src/week2/ds/unionfind/WeightedUnionFind.java
+++ src/week2/ds/unionfind/WeightedUnionFind.java
@@ -9,25 +9,71 @@ package week2.ds.unionfind;
 /**
  * @author Sandipan Aich
  */
-public class WeightedUnionFind //implements UnionFind 
-{
-//	public static void main(String[] args) {
-//		int size = 10;
-//
-//		UnionFind uf = new WeightedUnionFind(size);
-//
-//		uf.union(0, 2);
-//		uf.union(1, 3);
-//		uf.union(2, 4);
-//		uf.union(3, 5);
-//		uf.union(4, 6);
-//		uf.union(5, 7);
-//		uf.union(6, 8);
-//		uf.union(7, 9);
-//
-//		System.out.println("connected(0, 8) :: " + uf.connected(0, 8)); // true
-//		System.out.println("connected(1, 9) :: " + uf.connected(1, 9)); // true
-//		System.out.println("connected(0, 9) :: " + uf.connected(0, 9)); // false
-//	}
+public class WeightedUnionFind implements UnionFind {
+
+	private final int size;
+	private final int[] parent;
+	private final int[] weights;
+
+	public WeightedUnionFind(int size) {
+		this.size = size;
+		this.parent = new int[size];
+		this.weights = new int[size];
+
+		for (int i = 0; i < size; i++) {
+			parent[i] = i;
+			weights[i] = 1;
+		}
+	}
+
+	private int root(int u) {
+		while (parent[u] != u)
+			u = parent[u];
+		return u;
+	}
+
+	@Override
+	public boolean connected(int u, int v) {
+		return root(u) == root(v);
+	}
+
+	@Override
+	public void union(int u, int v) {
+		if(connected(u, v))
+			return;
+		
+		int parentu = root(u);
+		int parentv = root(v);
+		
+		if(weights[parentu] < weights[parentv])
+		{
+			parent[parentu] = parentv;
+			weights[parentv] += weights[parentu];
+		}
+		else
+		{
+			parent[parentv] = parentu;
+			weights[parentu] += weights[parentv];			
+		}
+	}
+
+	public static void main(String[] args) {
+		int size = 10;
+
+		UnionFind uf = new WeightedUnionFind(size);
+
+		uf.union(0, 2);
+		uf.union(1, 3);
+		uf.union(2, 4);
+		uf.union(3, 5);
+		uf.union(4, 6);
+		uf.union(5, 7);
+		uf.union(6, 8);
+		uf.union(7, 9);
+
+		System.out.println("connected(0, 8) :: " + uf.connected(0, 8)); // true
+		System.out.println("connected(1, 9) :: " + uf.connected(1, 9)); // true
+		System.out.println("connected(0, 9) :: " + uf.connected(0, 9)); // false
+	}
 
 }
