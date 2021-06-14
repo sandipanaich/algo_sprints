c2c2047a5a4b02563c6e4df031c47b8a6b69fcfb
sandipanaich <sandipanaich@gmail.com>
2021-06-14 09:45:21 +0530

    Initial Draft of Heap Implementation.

---
+++ src/week2/ds/heaps/MaxHeap.java
@@ -0,0 +1,32 @@
+/**
+ * MaxHeap Implementation using arrays.
+ * 
+ * add O(log(n))
+ * remove O(log(n))
+ * initialization O(1) or O(n log(n)) // depends upon the Constructor used
+ */
+package week2.ds.heaps;
+
+/**
+ * @author Sandipan Aich
+ */
+public class MaxHeap<T extends Comparable<T>> {
+
+	public static void main(String[] args) {
+
+//		Integer[] data = new Integer[] { 1, 0, 2, 9, 3, 8, 4, 7, 5, 6 };
+//		MaxHeap<Integer> heap = new MaxHeap<Integer>(data);
+//
+//		while (!heap.isEmpty())
+//			System.out.print("_" + heap.remove() + "_");
+//
+//		System.out.println("\n++++++++++++++++++++++++++++++");
+//
+//		heap = new MaxHeap<Integer>(10);
+//		for (int i : data)
+//			heap.add(i);
+//
+//		while (!heap.isEmpty())
+//			System.out.print("_" + heap.remove() + "_");
+	}
+}
