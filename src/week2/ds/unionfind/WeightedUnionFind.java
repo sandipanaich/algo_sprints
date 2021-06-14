5fab5325cce3d5a2f852166aefe7c0f74734b036
sandipanaich <sandipanaich@gmail.com>
2021-06-14 09:42:44 +0530

    Initial Draft of Weighted Union Find.

---
+++ src/week2/ds/unionfind/WeightedUnionFind.java
@@ -0,0 +1,33 @@
+/**
+ * Weighted Union Find Implementation of Union Find.
+ * Initialization: O(n)
+ * Find: 0(log(n))
+ * Union: O(log(n))
+ */
+package week2.ds.unionfind;
+
+/**
+ * @author Sandipan Aich
+ */
+public class WeightedUnionFind //implements UnionFind 
+{
+//	public static void main(String[] args) {
+//		int size = 10;
+//
+//		UnionFind uf = new WeightedUnionFind(size);
+//
+//		uf.union(0, 2);
+//		uf.union(1, 3);
+//		uf.union(2, 4);
+//		uf.union(3, 5);
+//		uf.union(4, 6);
+//		uf.union(5, 7);
+//		uf.union(6, 8);
+//		uf.union(7, 9);
+//
+//		System.out.println("connected(0, 8) :: " + uf.connected(0, 8)); // true
+//		System.out.println("connected(1, 9) :: " + uf.connected(1, 9)); // true
+//		System.out.println("connected(0, 9) :: " + uf.connected(0, 9)); // false
+//	}
+
+}
