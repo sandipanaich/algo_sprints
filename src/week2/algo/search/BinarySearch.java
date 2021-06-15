1d9db6c3dda35513ff40a19e85b0059de193a292
sandipanaich <sandipanaich@gmail.com>
2021-06-15 09:35:23 +0530

    Binary Search Implementation.

---
+++ src/week2/algo/search/BinarySearch.java
@@ -0,0 +1,19 @@
+/**
+ * Binary Search Implementation.
+ */
+package week2.algo.search;
+
+/**
+ * @author Sandipan Aich
+ */
+public class BinarySearch {
+
+	public static void main(String[] args) {
+
+		Integer[] data = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+//		for (int i = 0; i < 20; i++)
+//			System.out.println(i + " :: " + search(data, i));
+	}
+
+}
