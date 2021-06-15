55382194835e687e9e99debef13eaec85ab98eed
sandipanaich <sandipanaich@gmail.com>
2021-06-15 09:34:30 +0530

    Bubble Sort Implementation.

---
+++ src/week2/algo/sort/BubbleSort.java
@@ -0,0 +1,24 @@
+/**
+ * Bubble Sort Implementation.
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(1) // In-place Algorithm
+ * Stable: YES
+ */
+package week2.algo.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author Sandipan Aich
+ */
+public class BubbleSort {
+
+	public static void main(String[] args) {
+
+		Integer[] data = new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+//		String[] data = new String[] { "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A", };
+//		sort(data);
+		System.out.println(Arrays.toString(data));
+	}
+
+}
