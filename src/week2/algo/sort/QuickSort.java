58d79363e391cee6af11cc5089f2ef37b97096cd
sandipanaich <sandipanaich@gmail.com>
2021-06-15 09:30:12 +0530

    QuickSort Implementation.

---
+++ src/week2/algo/sort/QuickSort.java
@@ -0,0 +1,22 @@
+/**
+ * Quick Sort Implementation.
+ * Time Complexity: O(n log(n))
+ * Space Complexity: O(log(n))
+ */
+package week2.algo.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author Sandipan Aich
+ */
+public class QuickSort {
+
+	public static void main(String[] args) {
+		Integer[] data = new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+//		String[] data = new String[] { "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A", };
+//		sort(data);
+		System.out.println(Arrays.toString(data));
+	}
+
+}
