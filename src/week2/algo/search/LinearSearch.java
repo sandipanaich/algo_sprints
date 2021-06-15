c0b67717ed79dcfd4ccbaeb3ece3f687ea177aa1
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2021-06-15 10:20:55 +0530

    Linear Search Implementation.

--- src/week2/algo/search/LinearSearch.java
+++ src/week2/algo/search/LinearSearch.java
@@ -11,12 +11,25 @@ package week2.algo.search;
  */
 public class LinearSearch<T> {
 
+	public static final <T> int search(T[] data, T element)
+	{
+		int length = data.length;
+		
+		for(int i = 0; i < length; i++)
+		{
+			if(element.equals(data[i]))
+				return i;
+		}
+
+		return -1;
+	}
+	
 	public static void main(String[] args) {
 		Integer[] data = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
 		for(int i = 0 ; i < 20; i++)
 		{
-//			System.out.println(i + " :: " + search(data, i));
+			System.out.println(i + " :: " + search(data, i));
 		}
 	}
 
