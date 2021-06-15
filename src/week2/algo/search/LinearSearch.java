f1c4de01bbdad9c878a2f20743b0b7691109bd62
sandipanaich <sandipanaich@gmail.com>
2021-06-15 09:36:16 +0530

    Linear Search Implementation.

---
+++ src/week2/algo/search/LinearSearch.java
@@ -0,0 +1,23 @@
+/**
+ * Linear Search Implementation.
+ * 
+ * Time complexity: O(n)
+ * Space Complexity: O(1)
+ */
+package week2.algo.search;
+
+/**
+ * @author Sandipan Aich
+ */
+public class LinearSearch<T> {
+
+	public static void main(String[] args) {
+		Integer[] data = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		for(int i = 0 ; i < 20; i++)
+		{
+//			System.out.println(i + " :: " + search(data, i));
+		}
+	}
+
+}
