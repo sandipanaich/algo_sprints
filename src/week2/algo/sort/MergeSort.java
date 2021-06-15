b4ec1c2993118e54d7a2d4100a369cee39b0fbe4
sandipanaich <sandipanaich@gmail.com>
2021-06-15 09:27:54 +0530

    MergeSort Implementation.

---
+++ src/week2/algo/sort/MergeSort.java
@@ -0,0 +1,20 @@
+/**
+ * Merge Sort Implementation.
+ * Time Complexity: O(n log(n))
+ * Best and Worst Casr: O(n log(n))
+ * Space Complexity: O(log(n))
+ */
+package week2.algo.sort;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+	public static void main(String[] args) {
+//		Integer[] data = new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+		String[] data = new String[] { "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A", };
+//		sort(data);
+		System.out.println(Arrays.toString(data));
+	}
+
+}
