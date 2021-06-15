07b894a80da5fa9063904c5fc593fa0c56c5ae35
sandipanaich <sandipanaich@gmail.com>
2021-06-15 09:28:54 +0530

    HeapSort Implementation.

---
+++ src/week2/algo/sort/HeapSort.java
@@ -0,0 +1,27 @@
+/**
+ * Heap Sort Implementation.
+ * Time Complexity: O(log(n))
+ * Space Complexity: O(1) // In-place Algorithm
+ * Stable: NO
+ */
+package week2.algo.sort;
+
+import java.util.Arrays;
+
+import week2.ds.heaps.MaxHeap;
+
+/**
+ * @author Sandipan Aich
+ */
+public class HeapSort {
+
+	public static void main(String[] args) {
+
+//		Integer[] data = new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+		String[] data = new String[] { "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A", };
+//		sort(data);
+		System.out.println(Arrays.toString(data));
+
+	}
+
+}
