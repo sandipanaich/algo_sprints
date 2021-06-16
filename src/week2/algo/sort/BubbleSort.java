3ebba4bcc8cb67f698cf9b619ce6e7a791d0bf57
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2021-06-16 08:19:11 +0530

    Bubble Sort Implementation.

--- src/week2/algo/sort/BubbleSort.java
+++ src/week2/algo/sort/BubbleSort.java
@@ -13,11 +13,33 @@ import java.util.Arrays;
  */
 public class BubbleSort {
 
+	public static <T extends Comparable<T>> void sort(T[] data) {
+		int size = data.length;
+		int last = size - 1;
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < last - i; j++) {
+				if (less(data[j + 1], data[j]))
+					swap(data, j, j + 1);
+			}
+		}
+	}
+
+	private static <T> void swap(T[] data, int i, int j) {
+		T temp = data[i];
+		data[i] = data[j];
+		data[j] = temp;
+	}
+
+	private static <T extends Comparable<T>> boolean less(T one, T two) {
+		return one.compareTo(two) < 0;
+	}
+
 	public static void main(String[] args) {
 
-		Integer[] data = new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-//		String[] data = new String[] { "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A", };
-//		sort(data);
+//		Integer[] data = new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+		String[] data = new String[] { "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A", };
+		sort(data);
 		System.out.println(Arrays.toString(data));
 	}
 
