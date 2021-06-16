7b45e768eca28dc7f6ee0a99446edfa9b95b0ba7
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2021-06-16 08:43:34 +0530

    Selection Sort Implementation.

--- src/week2/algo/sort/SelectionSort.java
+++ src/week2/algo/sort/SelectionSort.java
@@ -13,11 +13,38 @@ import java.util.Arrays;
  */
 public class SelectionSort {
 
+	public static <T extends Comparable<T>> void sort(T[] data)
+	{
+		int size = data.length;
+		for(int i = 0; i < size; i++)
+		{
+			int min = i;
+			for(int j = i + 1; j < size; j++)
+			{
+				if(less(data[j], data[min]))
+					min = j;
+			}
+			swap(data, i, min);
+		}
+	}
+	
+	public static <T> void swap(T[] data, int i , int j)
+	{
+		T temp = data[i];
+		data[i] = data[j];
+		data[j] = temp;
+	}
+	
+	public static <T extends Comparable<T>> boolean less(T one, T two)
+	{
+		return one.compareTo(two) < 0;
+	}
+	
 	public static void main(String[] args) {
 
-//		Integer[] data = new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-		String[] data = new String[] { "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A", };
-//		sort(data);
+		Integer[] data = new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+//		String[] data = new String[] { "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A", };
+		sort(data);
 		System.out.println(Arrays.toString(data));
 	}
 
