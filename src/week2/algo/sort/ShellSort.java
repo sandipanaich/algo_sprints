2e5dd120bbb1c500ebf9425c8113b53c6521bdf2
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2021-06-16 08:58:28 +0530

    Shell Sort Implementation.

--- src/week2/algo/sort/ShellSort.java
+++ src/week2/algo/sort/ShellSort.java
@@ -13,11 +13,39 @@ import java.util.Arrays;
  */
 public class ShellSort {
 
+	public static <T extends Comparable<T>> void sort(T[] data) {
+
+		int size = data.length;
+		int gap = 0;
+
+		for (int i = 1; i < size; i = 3 * gap + 1, gap++)
+			;
+
+		while (gap > 0) {
+			for (int i = 0, j = gap; j < size; i++, j++) {
+				if (less(data[j], data[i]))
+					swap(data, i, j);
+			}
+			gap--;
+		}
+
+	}
+
+	private static <T> void swap(T[] data, int i, int j) {
+		T temp = data[i];
+		data[i] = data[j];
+		data[j] = temp;
+	}
+
+	private static <T extends Comparable<T>> boolean less(T one, T two) {
+		return one.compareTo(two) < 0;
+	}
+
 	public static void main(String[] args) {
 
-		Integer[] data = new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-//		String[] data = new String[] { "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A", };
-//		sort(data);
+//		Integer[] data = new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+		String[] data = new String[] { "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A", };
+		sort(data);
 		System.out.println(Arrays.toString(data));
 	}
 
