c8e48d796581345aa7fde361471f1ffadbeba6b2
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2021-06-16 08:30:24 +0530

    Insertion Sort Implementation.

--- src/week2/algo/sort/InsertionSort.java
+++ src/week2/algo/sort/InsertionSort.java
@@ -14,11 +14,32 @@ import java.util.Arrays;
  */
 public class InsertionSort {
 
+	public static <T extends Comparable<T>> void sort(T[] data) {
+
+		int size = data.length;
+
+		for (int i = 1; i < size; i++) {
+
+			int j = i - 1;
+			T element = data[i];
+
+			while (j >= 0 && less(element, data[j])) {
+				data[j + 1] = data[j];
+				j--;
+			}
+			data[j + 1] = element;
+		}
+	}
+
+	private static <T extends Comparable<T>> boolean less(T one, T two) {
+		return one.compareTo(two) < 0;
+	}
+
 	public static void main(String[] args) {
 
-		Integer[] data = new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-//		String[] data = new String[] { "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A", };
-//		sort(data);
+//		Integer[] data = new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+		String[] data = new String[] { "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A", };
+		sort(data);
 		System.out.println(Arrays.toString(data));
 	}
 
