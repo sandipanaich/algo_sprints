d63c67e82be6a3273901c1e995b261e26cc945e0
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2021-06-17 09:37:27 +0530

    Heap Sort Implementation.

--- src/week2/algo/sort/HeapSort.java
+++ src/week2/algo/sort/HeapSort.java
@@ -15,11 +15,24 @@ import week2.ds.heaps.MaxHeap;
  */
 public class HeapSort {
 
+	public static <T extends Comparable<T>> void sort(T[] data) {
+		
+		int size = data.length;
+		MaxHeap<T> heap = new MaxHeap<T>(data);
+		
+		while(size > 0)
+		{
+			int last = size - 1;
+			data[last] = heap.remove();
+			size--;
+		}
+	}
+
 	public static void main(String[] args) {
 
-//		Integer[] data = new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-		String[] data = new String[] { "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A", };
-//		sort(data);
+		Integer[] data = new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+//		String[] data = new String[] { "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A", };
+		sort(data);
 		System.out.println(Arrays.toString(data));
 
 	}
