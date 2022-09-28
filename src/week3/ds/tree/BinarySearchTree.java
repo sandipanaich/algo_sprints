0842b4c8d87ce92882cb190d66648a5a42b7e673
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2022-09-28 21:00:53 +0530

    Binary Tree Traversals. Closes #122.

--- src/week3/ds/tree/BinarySearchTree.java
+++ src/week3/ds/tree/BinarySearchTree.java
@@ -33,10 +33,71 @@ public class BinarySearchTree<T extends Comparable<T>> {
 			parentNode.left(node);
 		else
 			parentNode.right(node);
-		
+
 		return this;
 	}
 
+	// ------------------------------------------------------------------------------------------------
+	public final List<Node<T>> postOrderTraversal() {
+
+		List<Node<T>> traversalList = new ArrayList<>();
+
+		Node<T> node = root;
+		postOrder(node, traversalList);
+		return traversalList;
+	}
+
+	private final void postOrder(Node<T> node, List<Node<T>> traversalList) {
+
+		if (node == null)
+			return;
+
+		postOrder(node.left(), traversalList);
+		postOrder(node.right(), traversalList);
+		traversalList.add(node);
+	}
+
+	// ------------------------------------------------------------------------------------------------
+	public final List<Node<T>> preOrderTraversal() {
+
+		List<Node<T>> traversalList = new ArrayList<>();
+
+		Node<T> node = root;
+		preOrder(node, traversalList);
+		return traversalList;
+	}
+
+	private final void preOrder(Node<T> node, List<Node<T>> traversalList) {
+
+		if (node == null)
+			return;
+
+		traversalList.add(node);
+		preOrder(node.left(), traversalList);
+		preOrder(node.right(), traversalList);
+	}
+
+	// ------------------------------------------------------------------------------------------------
+	public final List<Node<T>> inorderTraversal() {
+
+		List<Node<T>> traversalList = new ArrayList<>();
+
+		Node<T> node = root;
+		inorder(node, traversalList);
+		return traversalList;
+	}
+
+	private final void inorder(Node<T> node, List<Node<T>> traversalList) {
+
+		if (node == null)
+			return;
+
+		inorder(node.left(), traversalList);
+		traversalList.add(node);
+		inorder(node.right(), traversalList);
+	}
+
+	// ------------------------------------------------------------------------------------------------
 	public final List<Node<T>> levelOrderTraversal() {
 		List<Node<T>> traversalList = new ArrayList<>();
 		Queue<Node<T>> queue = new LinkedList<>();
@@ -56,11 +117,17 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		return traversalList;
 	}
 
+//------------------------------------------------------------------------------------------------
 	public static void main(String[] args) {
 		BinarySearchTree<Integer> tree = new BinarySearchTree<>();
 
 		tree.add(100).add(50).add(150).add(25).add(75).add(125).add(175).add(200);
 		tree.levelOrderTraversal().stream().forEach(a -> System.out.print(a + " "));
+		System.out.println();
+		tree.preOrderTraversal().stream().forEach(a -> System.out.print(a + " "));
+		System.out.println();
+		tree.postOrderTraversal().stream().forEach(a -> System.out.print(a + " "));
+		System.out.println();
+		tree.inorderTraversal().stream().forEach(a -> System.out.print(a + " "));
 	}
-
 }
