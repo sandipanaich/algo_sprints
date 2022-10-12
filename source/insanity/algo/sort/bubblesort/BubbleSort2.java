12d90fb3e8aadc340c069320cdb406810717f23a
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2022-10-12 07:29:11 +0530

    Code Refactoring and Bubble Sort.

---
+++ source/insanity/algo/sort/bubblesort/BubbleSort2.java
@@ -0,0 +1,35 @@
+package insanity.algo.sort.bubblesort;
+
+import java.util.Arrays;
+
+public class BubbleSort2 {
+
+	public static final <T extends Comparable<T>> T[] sort(T[] array) {
+
+		int len = array.length;
+		int last = len - 1;
+
+		for (int i = last; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				if (array[j].compareTo(array[j + 1]) > 0)
+					swap(array, j, j + 1);
+			}
+		}
+
+		return array;
+	}
+
+	private static final <T> void swap(T[] array, int i, int j) {
+		T temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(sort(new Integer[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 })));
+		System.out.println(Arrays.toString(sort(new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 })));
+		System.out.println(Arrays.toString(sort(new Integer[] { 0, 3, 4, 7, 8, 9, 6, 5, 2, 1 })));
+		System.out.println(Arrays.toString(sort(new Integer[] { 1, 0, 3, 2, 4, 5, 7, 6, 9, 8 })));
+	}
+
+}
