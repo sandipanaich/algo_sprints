3edab24307047e31a21b5f479d6a7b6910a9d964
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2022-10-27 22:20:16 +0530

    29 october 2022 20 sorting algorithms

---
+++ source/insanity/algo/sort/insertionsort/InsertionSort20.java
@@ -0,0 +1,38 @@
+package insanity.algo.sort.insertionsort;
+
+import java.util.Arrays;
+
+public class InsertionSort20 {
+
+	public static final <T extends Comparable<T>> T[] sort(T[] array) {
+
+		if (array == null || array.length < 2)
+			return array;
+
+		int length = array.length;
+
+		for (int i = 1; i < length; i++) {
+			T element = array[i];
+			int j = i - 1;
+
+			while (j >= 0 && less(element, array[j])) {
+				array[j + 1] = array[j];
+				j -= 1;
+			}
+			array[j+1] = element;
+		}
+		return array;
+	}
+
+	private static final <T extends Comparable<T>> boolean less(T one, T two) {
+		return one.compareTo(two) < 0;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(sort(new Integer[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 })));
+		System.out.println(Arrays.toString(sort(new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 })));
+		System.out.println(Arrays.toString(sort(new Integer[] { 0, 3, 4, 7, 8, 9, 6, 5, 2, 1 })));
+		System.out.println(Arrays.toString(sort(new Integer[] { 1, 0, 3, 2, 4, 5, 7, 6, 9, 8 })));
+	}
+
+}
