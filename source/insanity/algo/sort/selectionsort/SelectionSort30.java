bc26cfc05ea97b7b95e8b82899778ededa81c974
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2022-11-08 09:43:25 -0500

    09 November 2022 30 Sorting Algorithms.

---
+++ source/insanity/algo/sort/selectionsort/SelectionSort30.java
@@ -0,0 +1,44 @@
+package insanity.algo.sort.selectionsort;
+
+import java.util.Arrays;
+
+public class SelectionSort30 {
+
+	public static final <T extends Comparable<T>> T[] sort(T[] array) {
+
+		if (array == null || array.length < 2)
+			return array;
+
+		int length = array.length;
+		int last = length - 1;
+
+		for (int i = 0; i < last; i++) {
+			int least = i;
+			for (int j = i + 1; j < length; j++) {
+				if (less(array[j], array[least]))
+					least = j;
+			}
+			if (least != i)
+				swap(array, least, i);
+		}
+		return array;
+	}
+
+	private static final <T extends Comparable<T>> boolean less(T one, T two) {
+		return one.compareTo(two) < 0;
+	}
+
+	private static final <T> void swap(T[] array, int i, int j) {
+		T temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(sort(new Integer[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 })));
+		System.out.println(Arrays.toString(sort(new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 })));
+		System.out.println(Arrays.toString(sort(new Integer[] { 0, 3, 4, 7, 8, 9, 6, 5, 2, 1 })));
+		System.out.println(Arrays.toString(sort(new Integer[] { 1, 0, 3, 2, 4, 5, 7, 6, 9, 8 })));
+	}
+
+}
