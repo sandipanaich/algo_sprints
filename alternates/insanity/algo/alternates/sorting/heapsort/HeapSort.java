1bf0eab704167039883038258dec63116b8f03fa
Sandipan Aich <Sandipan.Aich@Honeywell.com>
2022-11-23 11:39:29 -0500

    Sorting Algorithms 23 November 2022. #3.

---
+++ alternates/insanity/algo/alternates/sorting/heapsort/HeapSort.java
@@ -0,0 +1,84 @@
+package insanity.algo.alternates.sorting.heapsort;
+
+import java.util.Arrays;
+
+public class HeapSort {
+
+	public static final <T extends Comparable<T>> T[] sort(T[] array) {
+
+		if (array == null || array.length < 2)
+			return array;
+
+		int length = array.length;
+		int last = length - 1;
+		final MaxHeap<T> heap = new MaxHeap<>(array);
+
+		for (int index = last; index >= 0; index--)
+			array[index] = heap.remove();
+
+		return array;
+	}
+
+	private static final class MaxHeap<E extends Comparable<E>> {
+
+		private int length;
+		private final E[] heap;
+
+		public MaxHeap(E[] array) {
+			this.length = array.length;
+			this.heap = array;
+			heapify();
+		}
+
+		private final void heapify() {
+
+			int last = this.length;
+			for (int index = last / 2; index >= 0; index--)
+				sink(index);
+		}
+
+		private final E remove() {
+			E element = this.heap[0];
+			int last = this.length - 1;
+			swap(this.heap, 0, last);
+			this.length -= 1;
+			sink(0);
+			return element;
+		}
+
+		private final void sink(int index) {
+
+			int left = 2 * index + 1;
+			int right = 2 * index + 2;
+
+			left = (left < this.length) ? left : index;
+			right = (right < this.length) ? right : index;
+
+			int max = less(this.heap[left], this.heap[right]) ? right : left;
+			max = less(this.heap[index], this.heap[max]) ? max : index;
+
+			if (max != index) {
+				swap(this.heap, max, index);
+				sink(max);
+			}
+		}
+
+		private final void swap(E[] array, int i, int j) {
+			E temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+
+		private final boolean less(E one, E two) {
+			return one.compareTo(two) < 0;
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(sort(new Integer[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 })));
+		System.out.println(Arrays.toString(sort(new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 })));
+		System.out.println(Arrays.toString(sort(new Integer[] { 0, 3, 4, 7, 8, 9, 6, 5, 2, 1 })));
+		System.out.println(Arrays.toString(sort(new Integer[] { 1, 0, 3, 2, 4, 5, 7, 6, 9, 8 })));
+	}
+
+}
---
+++ alternates/insanity/algo/alternates/sorting/mergesort/MergeSort3.java
@@ -0,0 +1,60 @@
+package insanity.algo.alternates.sorting.mergesort;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MergeSort3 {
+
+	public static final <T extends Comparable<T>> T[] sort(T[] array) {
+
+		if (array == null || array.length < 2)
+			return array;
+
+		int length = array.length;
+		int last = length - 1;
+		final T[] aux = (T[]) Array.newInstance(array[0].getClass(), length);
+		return sort(array, aux, 0, last);
+	}
+
+	private static final <T extends Comparable<T>> T[] sort(T[] array, T[] aux, int left, int right) {
+
+		if (left < right) {
+			final int mid = (left + right) / 2;
+			sort(array, aux, left, mid);
+			sort(array, aux, mid + 1, right);
+			merge(array, aux, left, mid, right);
+		}
+		return array;
+	}
+
+	private static final <T extends Comparable<T>> void merge(T[] array, T[] aux, int left, int mid, int right) {
+		int index = left;
+		int i = left;
+		int j = mid + 1;
+
+		while (true) {
+			if (i > mid && j > right)
+				break;
+			else if (i > mid)
+				aux[index++] = array[j++];
+			else if (j > right)
+				aux[index++] = array[i++];
+			else
+				aux[index++] = less(array[i], array[j]) ? array[i++] : array[j++];
+		}
+		for (index = left; index <= right; index++)
+			array[index] = aux[index];
+	}
+
+	private static final <T extends Comparable<T>> boolean less(T one, T two) {
+		return one.compareTo(two) < 0;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(sort(new Integer[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 })));
+		System.out.println(Arrays.toString(sort(new Integer[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 })));
+		System.out.println(Arrays.toString(sort(new Integer[] { 0, 3, 4, 7, 8, 9, 6, 5, 2, 1 })));
+		System.out.println(Arrays.toString(sort(new Integer[] { 1, 0, 3, 2, 4, 5, 7, 6, 9, 8 })));
+	}
+
+}
